3afa389c83e573d79feb05b4301e4d33e9cba1e6
kao.ring <kao.ring@yahoo.com>
2020-07-07 00:33:46 -0400

    add questions

---
+++ script.js
@@ -0,0 +1,56 @@
+//array question
+
+//interval
+
+var questions = [
+    {
+    q:"Commonly data types DO NOT includes;"
+    a:["strings","numbers","array","boolean"]
+    correct:"array"
+},
+{
+q:"The condition in an if / else statement is enclosed within_____."
+a:["quotation","square brackets","parentheses","curly brackets"]
+correct:"parentheses"
+},
+{
+    q:
+    a:
+    correct:
+    },
+    {
+        q:
+        a:
+        correct:
+        },
+        {
+            q:
+            a:
+            correct:
+            },
+            {
+                q:
+                a:
+                correct:
+                },
+                {
+                    q:
+                    a:
+                    correct:
+                    },
+                    {
+                        q:
+                        a:
+                        correct:
+                        },
+                        {
+                            q:
+                            a:
+                            correct:
+                            },
+                            {
+                                q:
+                                a:
+                                correct:
+                                },
+]
