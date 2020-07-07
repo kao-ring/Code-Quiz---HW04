58f93b078175f262ed2dfae82f9467a0388dbf5d
kao.ring <kao.ring@yahoo.com>
2020-07-07 00:58:01 -0400

    add questions2

--- script.js
+++ script.js
@@ -4,53 +4,53 @@
 
 var questions = [
     {
-    q:"Commonly data types DO NOT includes;"
-    a:["strings","numbers","array","boolean"]
+    q:"Commonly data types DO NOT includes;",
+    a:["strings","numbers","array","boolean"],
     correct:"array"
 },
 {
-q:"The condition in an if / else statement is enclosed within_____."
-a:["quotation","square brackets","parentheses","curly brackets"]
+q:"The condition in an if / else statement is enclosed within _____.",
+a:["quotation","square brackets","parentheses","curly brackets"],
 correct:"parentheses"
 },
 {
-    q:
-    a:
-    correct:
+    q:"Arrays in javascript canbe used  to store ______.",
+    a:["numbers and strings","other arrays","boolean", "all of the above"],
+    correct:"all of the above"
     },
     {
-        q:
-        a:
-        correct:
+        q:"Strings value must be enclosed within _____  when being assigned to variables.",
+        a:"quotation","square brackets","parentheses","curly brackets",
+        correct:"quotation"
         },
         {
-            q:
-            a:
-            correct:
+            q:"A very useful tool used during development adn debugging for printing content to the de bugger is;",
+            a:["javascript","Terminal/Bash","for loops","console.log"],
+            correct:"console.log"
             },
             {
-                q:
-                a:
-                correct:
+                q:"Inside which HTML element do we put the JavaScript?",
+                a:"<script>","<scripting>","<js>","<javascript>",
+                correct:"<script>"
                 },
                 {
-                    q:
-                    a:
-                    correct:
+                    q:"What is the correct JavaScript syntax to change the content of the HTML element below? <p id='demo'>This is a demonstration.</p>",
+                    a:[" document.getElementByName('p').innerHTML = 'Hello World!';"," document.getElement('p').innerHTML = 'Hello World!';"," #demo.innerHTML = 'Hello World!';"," document.getElementById('demo').innerHTML = 'Hello World!';"],
+                    correct:" document.getElementById('demo').innerHTML = 'Hello World!';"
                     },
                     {
-                        q:
-                        a:
-                        correct:
+                        q:"How do you write 'Hello World' in an alert box?",
+                        a:[" alert('Hello World');","msg('Hello World');","alertBox('Hello World');","console.log('Hello World');"],
+                        correct:" alert('Hello World');"
                         },
                         {
-                            q:
-                            a:
-                            correct:
+                            q:"How do you create a function in JavaScript?",
+                            a:["function=myFunction()","function:myfunction()","function myFunction()","var function=function"],
+                            correct:"function myFunction()"
                             },
                             {
-                                q:
-                                a:
-                                correct:
+                                q:"How can you add a comment in a JavaScript?",
+                                a:["//This is a comment","'This is a comment","<!--This is a comment-->","%This is a comment%"],
+                                correct:"//This is a comment"
                                 },
 ]
